c6bae0c59ce44a29289dcbec2ff4acd74411b9c8
Muhammad-Abid11 <abidkhan11845@gmail.com>
2024-01-26 00:29:44 +0500

    Fetch-Data and Add it in TodoList

--- pages/todos.tsx
+++ pages/todos.tsx
@@ -2,19 +2,49 @@
 import React, { useEffect, useState } from 'react'
 
 export default function Todos() {
-    const [todos, setTodos] = useState<string[]>([])
+    // const [todos, setTodos] = useState<string[]>([])
+    const [todos, setTodos] = useState<any[]>([])//for "api" q k api sy "string" k ilawa bhi types arhen hen agar "string" rha too nichy "value.title" me red line ayegi
     const [inputValue, setInputValue] = useState<string>("")
     const handleChange = (value: string) => {
         // console.log(value)
         setInputValue(value)
     }
 
+    //Class 10 API-Todo 00:00:05
+    // Getdata From API and render it in Todo-list
+    useEffect(() => {
+        console.log("API Calling....")
+        getdata()
+    }, [])
+    const getdata = () => {
+        fetch('https://jsonplaceholder.typicode.com/todos')
+            .then(response => response.json())
+            .then(json => setTodos(json))
+        // .then(json => console.log(json))
+    }
+    // console.log("Todo from API", todos)
+
     const addTodo = () => {
-        console.log("input", inputValue)
-        todos.push(inputValue)
+        /*     
+            // console.log("input", inputValue)
+            todos.push(inputValue)
+            setTodos(todos)
+            // console.log("todo", todos)
+            setInputValue("")
+     */
+
+        // for API 
+        //Class 10 API-Todo 00:21:47
+        let obj: object = {
+            completed: true,
+            title: inputValue,
+            userId: 1,
+            id: todos.length + 1
+        }
+        todos.push(obj)
         setTodos(todos)
-        console.log("todo", todos)
         setInputValue("")
+
     }
 
     return (
@@ -29,8 +59,14 @@ export default function Todos() {
 
             <ul>
                 {todos.map((value, index) => {
+                    const colored = value.completed;
                     return (
-                        <li key={index}>{value}</li>//display/UI component
+                        // <li key={index}>{value}</li>//display/UI component without API
+
+                        //Class 10 API-Todo 00:00:05
+                        <li key={index} style={{ color: colored ? "green" : "red" }}>
+                            {value.title}
+                        </li>//for API render 
                     )
                 })}
             </ul>
